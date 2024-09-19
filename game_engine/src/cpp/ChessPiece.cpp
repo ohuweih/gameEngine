77fbc0e60484533b322e4d1fde82356bee936090
Omar Huweih <ohuweih@gmail.com>
2024-09-18 22:25:38 -0700

    Adding move logger logic to pieces

--- game_engine/src/cpp/ChessPiece.cpp
+++ game_engine/src/cpp/ChessPiece.cpp
@@ -36,6 +36,10 @@ std::string Pawn::getName() const {
     return (color == Color::White) ? "White Pawn" : "Black Pawn";
 }
 
+PieceType Pawn::getType() const {
+    return PieceType::Pawn;
+}
+
 
 bool Rook::isValidMove(int newRow, int newCol, GameState& game_state) const {
     if (row != newRow && col != newCol) {
@@ -71,6 +75,10 @@ std::string Rook::getName() const {
     return (color == Color::White) ? "White Rook" : "Black Rook";
 }
 
+PieceType Rook::getType() const {
+    return PieceType::Rook;
+}
+
 
 bool Knight::isValidMove(int newRow, int newCol, GameState& game_state) const {
     int rowDiff = std::abs(row - newRow);
@@ -96,44 +104,100 @@ std::string Knight::getName() const {
     return (color == Color::White) ? "White Knight" : "Black Knight";
 }
 
+PieceType Knight::getType() const {
+    return PieceType::Knight;
+}
+
 
 bool Bishop::isValidMove(int newRow, int newCol, GameState& game_state) const {
-    int rowDirection = (color == Color::White) ? -1 : 1;
-    if (newCol == col && newRow == row + rowDirection) {
-        if (game_state.board[newRow][newCol] == nullptr) {
-             return true;
-        }
+    Piece* targetPiece = game_state.board[newRow][newCol];
+
+    if (std::abs(row - newRow) != std::abs(col - newCol)) {
+        return false;
     }
+
+    // Check if path is clear
+    int rowStep = (row < newRow) ? 1 : -1;
+    int colStep = (col < newCol) ? 1 : -1;
+    int moveRow = row + rowStep;
+    int moveCol = col + colStep;
+    while (moveRow != newRow && moveCol != newCol) {
+        if (game_state.board[moveRow][moveCol] != nullptr) {
+            return false;
+        }
+        moveRow += rowStep;
+        moveCol += colStep;
+    }
+
+    if (targetPiece == nullptr || targetPiece->getColor() != color) {
+        return true;
+    }
+    
+    return false;
 }
 
 std::string Bishop::getName() const {
-    return (color == Color::White) ? "White Pawn" : "Black Pawn";
+    return (color == Color::White) ? "White Bishop" : "Black Bishop";
+}
+
+PieceType Bishop::getType() const {
+    return PieceType::Bishop;
 }
 
 
 bool Queen::isValidMove(int newRow, int newCol, GameState& game_state) const {
-    int rowDirection = (color == Color::White) ? -1 : 1;
-    if (newCol == col && newRow == row + rowDirection) {
-        if (game_state.board[newRow][newCol] == nullptr) {
-             return true;
-        }
+    Rook tempRook(color, row, col);
+    Bishop tempBishop(color, row, col);
+
+    if (tempRook.isValidMove(newRow, newCol, game_state) ||
+        tempBishop.isValidMove(newRow, newCol, game_state)) {
+        return true;
     }
+
+    return false;
 }
 
 std::string Queen::getName() const {
-    return (color == Color::White) ? "White Pawn" : "Black Pawn";
+    return (color == Color::White) ? "White Queen" : "Black Queen";
+}
+
+PieceType Queen::getType() const {
+    return PieceType::Queen;
 }
 
 
 bool King::isValidMove(int newRow, int newCol, GameState& game_state) const {
-    int rowDirection = (color == Color::White) ? -1 : 1;
-    if (newCol == col && newRow == row + rowDirection) {
-        if (game_state.board[newRow][newCol] == nullptr) {
-             return true;
+    int rowDiff = std::abs(row - newRow);
+    int colDiff = std::abs(col - newCol);
+    Piece* targetPiece = game_state.board[newRow][newCol];
+
+    // Ensure the King moves only one square in any direction
+    if (rowDiff <= 1 && colDiff <= 1) {
+        // If the destination square is empty, it's a valid move
+        if (targetPiece == nullptr) {
+            return true;
+        }
+        // If there's an opponent's piece, it's a valid capture
+        else if (targetPiece->getColor() != color) {
+            return true;
         }
     }
+
+    // Castle logic
+    if (rowDiff == 0 && (colDiff == 2 || colDiff == -2)) {
+        if (SpecialState::canCastle(game_state, this)) {
+            return true; //Castling is allowed
+        }
+    }
+
+    // Invalid move
+    return false;
 }
 
 std::string King::getName() const {
-    return (color == Color::White) ? "White Pawn" : "Black Pawn";
+    return (color == Color::White) ? "White King" : "Black King";
+}
+
+PieceType King::getType() const {
+    return PieceType::King;
 }
